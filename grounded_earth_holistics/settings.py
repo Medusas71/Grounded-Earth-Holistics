2d08e38579e50fabef87685f82d69f634f5bf733
Medusas71 <medusas71@icloud.com>
2022-06-22 01:10:58 +0000

    Start of Deployment

--- grounded_earth_holistics/settings.py
+++ grounded_earth_holistics/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('GEH_SECRET_KEY', '')
+SECRET_KEY = 'django-insecure-1(lwk--an(l37l$^6(38j*&_&kgl6^=w4n8%+!-6=n9%^f802u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
