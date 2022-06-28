6956e8057412a9beff01e153a193366bb20cf568
Medusas71 <medusas71@icloud.com>
2022-06-28 15:09:59 +0000

    Added json files to upload products & categories

--- products/models.py
+++ products/models.py
@@ -29,7 +29,6 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    dimensions = models.CharField(max_length=254, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
